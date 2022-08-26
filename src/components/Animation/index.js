3ababb7624f23eda4088fef6ea83d1202ca4994b
Joshua Kummer <kummer.joshua@outlook.com>
2022-08-26 23:27:49 +0200

    Add net animation

--- src/components/Animation/index.js
+++ src/components/Animation/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import styles from "./styles.module.css";
 
 import * as THREE from "three";
-import BIRDS from "vanta/dist/vanta.birds.min";
+import NET from "vanta/dist/vanta.net.min";
 
 export const BackgroundAnimation = () => {
   const [vantaEffect, setVantaEffect] = useState(0);
@@ -11,7 +11,7 @@ export const BackgroundAnimation = () => {
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(
-        BIRDS({
+        NET({
           el: vantaRef.current,
           THREE: THREE,
           mouseControls: true,
@@ -19,13 +19,11 @@ export const BackgroundAnimation = () => {
           gyroControls: false,
           scale: 1.0,
           scaleMobile: 1.0,
-          color1: "#FC1DAE",
-          color2: "#00F6FF",
-          colorMode: "lerpGradient",
-          birdSize: 0.9,
-          quantity: 4,
-          separation: 100,
-          backgroundAlpha: 0
+          color: 0xFC1DAE,
+          backgroundColor: 0x1E1E1E,
+          points: 5.0,
+          maxDistance: 25.0,
+          spacing: 20.0,
         })
       );
     }
